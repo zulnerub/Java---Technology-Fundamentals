2adf2b7bb4ffbd95e126028871e23b4fd872e77a
zulnerub <49581585+zulnerub@users.noreply.github.com>
2020-01-08 20:14:39 +0200

    Input -> number double. Output -> if higher than 2.99

---
+++ higherThanThree.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class passed {
+		
+		public static void main(String[] args) {
+			Scanner scanner = new Scanner(System.in);
+			
+			double a = Double.parseDouble(scanner.nextLine());
+			
+			if (a >= 3.00) {
+				System.out.println("Passed!");
+			}
+			
+		}
+
+}
