f2f4c21bcb791aa1892dd94dfc755a291139b8fc
zulnerub <49581585+zulnerub@users.noreply.github.com>
2020-01-08 20:24:48 +0200

    Create multiplicationTableWhileLoop.java

---
+++ multiplicationTableWhileLoop.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class multy {
+		public static void main(String[] args) {
+			Scanner scanner = new Scanner(System.in);
+			
+			int num = Integer.parseInt(scanner.nextLine());
+			int times = Integer.parseInt(scanner.nextLine());
+			
+			do {
+				
+				System.out.printf("%d X %d = %d%n", num, times,num * times);
+				
+				
+				times++;
+				
+			} while (times <= 10);
+			
+		}
+
+}
