2bb4ec890018bbf5b253b0dbb1f9b86af3e00c4a
zulnerub <49581585+zulnerub@users.noreply.github.com>
2020-01-08 20:22:14 +0200

    Create sumOddNums.java

---
+++ sumOddNums.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class sumodd {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = Integer.parseInt(scanner.nextLine());
+		int count = 0;
+		int sum = 0;
+		for (int i = 1; i <= 100;i++ ) {
+			if (i % 2 != 0 && count < n) {
+				count++;
+				System.out.println(i);
+				sum += i;
+			}
+		}
+		System.out.println("Sum: " + sum);
+	}
+}
