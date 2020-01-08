cb8cbcec0c0bf9e2e92928d4d8673e34be3b07a9
zulnerub <49581585+zulnerub@users.noreply.github.com>
2020-01-08 20:21:45 +0200

    Create divisableByThree.java

---
+++ divisableByThree.java
@@ -0,0 +1,10 @@
+public class divide3 {
+	public static void main(String[] args) {
+		int a = 1;
+		for (int i = 1; i <= 100; i++) {
+			if (i % 3 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
