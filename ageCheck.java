2fdf0fcd7a560e3e7c2b4f7c37bb90e134bc5404
zulnerub <49581585+zulnerub@users.noreply.github.com>
2020-01-08 20:27:01 +0200

    Create ageCheck.java

---
+++ ageCheck.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Agaes_01 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int age = Integer.parseInt(scanner.nextLine());
+	
+		if (age >= 0 && age < 3){
+			System.out.println("baby");
+		}else if (age >= 3 && age < 14) {
+			System.out.println("child");
+		}else if (age >= 14 && age < 20) {
+			System.out.println("teenager");
+		}else if (age >= 20 && age < 66){
+			System.out.println("adult");
+		}else if (age >= 66 ){
+			System.out.println("elder");
+		}
+	}
+}
